c20878be65dfec2b9168ba69226ae614ae384216
Ziyang <tonyziyangzhao@gmail.com>
2023-05-18 23:12:25 +1000

    create seatbutton

---
+++ assignment3/SeatButton.swift
@@ -0,0 +1,20 @@
+//
+//  SeatButton.swift
+//  assignment3
+//
+//  Created by Tony on 18/5/2023.
+//
+
+import UIKit
+
+class SeatButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
