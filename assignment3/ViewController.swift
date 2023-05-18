7b6df03b055016dbbd02bf1f793f5ca7a55d745f
JieKang <jie.kang-1@student.uts.edu.au>
2023-05-18 15:55:43 +1000

    Initial Commit

---
+++ assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment3
+//
+//  Created by 康杰 on 18/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assignment3UITests/assignment3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  assignment3UITestsLaunchTests.swift
+//  assignment3UITests
+//
+//  Created by 康杰 on 18/5/2023.
+//
+
+import XCTest
+
+final class assignment3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
