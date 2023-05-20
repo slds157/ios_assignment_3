988a9794bdc1362cc9c4d37be326e6d690611672
Zheng Yuan <zheng.yuan-1@student.uts.edu.au>
2023-05-20 17:49:31 +1000

    files create

---
+++ assignment3/TicketViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TicketViewController.swift
+//  assignment3
+//
+//  Created by Zheng Yuan on 2023-05-20.
+//
+
+import UIKit
+
+class TicketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
