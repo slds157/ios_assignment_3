e5420c140413f22d2c1c90748fe608648d53d07b
Ziyang <tonyziyangzhao@gmail.com>
2023-05-20 17:29:14 +1000

    Update SeatButton

--- assignment3/SeatButton.swift
+++ assignment3/SeatButton.swift
@@ -2,19 +2,41 @@
 //  SeatButton.swift
 //  assignment3
 //
-//  Created by Tony on 18/5/2023.
+//  Created by Tony on 20/5/2023.
 //
 
 import UIKit
 
 class SeatButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var seat: Seat!
+    
+    func configure(seat: Seat){
+        self.seat = seat
+        updateAppearance()
     }
-    */
+    
+    func updateAppearance() {
+        switch seat.status {
+        case .available:
+            backgroundColor = .green
+        case .occupied:
+            backgroundColor = .red
+        case .unavailable:
+            backgroundColor = .gray
+        }
+    }
 
+}
+
+struct Seat: Codable {
+    let row: Int
+    let column: Int
+    var status: SeatStatus
+}
+
+enum SeatStatus: Int, Codable {
+    case available
+    case occupied
+    case unavailable
 }
