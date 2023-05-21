5a6df64b53cfbd804c41f059dfe882815a3b3420
Zheng Yuan <zheng.yuan-1@student.uts.edu.au>
2023-05-21 16:43:24 +1000

    improve movie choose UI design

--- assignment3/MovieChooseViewController.swift
+++ assignment3/MovieChooseViewController.swift
@@ -21,23 +21,35 @@ class MovieChooseViewController: UIViewController, UIScrollViewDelegate {
     var currentImage : Int = 0
     var movieTitle : String?
     var movieTitleCollection : [String: String] = ["image1": "Evil Dead Rise", "image2": "Book Club: The Next Chapter", "image3": "Love Again", "image4": "Guardians of the Galaxy - Vol 3", "image5": "John Wick: Chapter 4"]
+    var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setImageSliding()
     }
     
     private func setImageSliding() {
-        let imageWidth = self.view.frame.size.width
-        let imageHeight = self.view.frame.size.height * 0.5
-        
-        scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: imageWidth, height: imageHeight))
+        let imageWidth = self.view.frame.size.width * 0.6
+        let imageHeight = self.view.frame.size.height * 0.4
+        let scrollViewX = (self.view.frame.size.width - imageWidth) / 2
+
+        let statusBarHeight: CGFloat
+        if #available(iOS 13.0, *) {
+            statusBarHeight = view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        } else {
+            statusBarHeight = UIApplication.shared.statusBarFrame.height
+        }
+        let topOffset: CGFloat = (self.navigationController?.navigationBar.frame.size.height ?? 0) + statusBarHeight
+        let scrollViewY = topOffset
+
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: scrollViewY, width: self.view.frame.size.width, height: imageHeight))
         scrollView.isPagingEnabled = true
-        scrollView.contentSize = CGSize(width: imageWidth * CGFloat(images.count), height: imageHeight)
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width * CGFloat(images.count), height: imageHeight)
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.delegate = self
-        
+
         for i in 0 ..< images.count {
-            let imageView = UIImageView(frame: CGRect(x: imageWidth * CGFloat(i), y: 0, width: imageWidth, height: imageHeight))
+            let imageView = UIImageView(frame: CGRect(x: self.view.frame.size.width * CGFloat(i) + scrollViewX, y: 0, width: imageWidth, height: imageHeight))
             guard let image = UIImage(named: images[i]) else {
                 continue
             }
@@ -47,21 +59,30 @@ class MovieChooseViewController: UIViewController, UIScrollViewDelegate {
             imageView.tag = i
             scrollView.addSubview(imageView)
         }
-        
+
         self.view.addSubview(scrollView)
+
+        // Create page control
+        pageControl = UIPageControl(frame: CGRect(x: scrollViewX, y: scrollView.frame.maxY, width: imageWidth, height: 20))
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = .lightGray
+        pageControl.currentPageIndicatorTintColor = .black
+        self.view.addSubview(pageControl)
     }
-        
+    
     @IBAction func chooseButtonCheck(_ sender: UIButton) {
         currentPageCalculation(scrollView)
     }
 
     func currentPageCalculation(_ scrollView: UIScrollView) {
         let pageWidth = scrollView.frame.size.width
-        currentImage = Int(scrollView.contentOffset.x / pageWidth)
+        currentImage = Int((scrollView.contentOffset.x + pageWidth / 2) / pageWidth)
         imageName = images[currentImage]
         if let imgName = imageName, let title = movieTitleCollection[imgName] {
-                self.movieTitle = title
-            }
+            self.movieTitle = title
+        }
+        pageControl.currentPage = currentImage
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
@@ -88,6 +109,4 @@ class MovieChooseViewController: UIViewController, UIScrollViewDelegate {
             attentionLabel.text = ""
         }
     }
-    }
-
-
+}
